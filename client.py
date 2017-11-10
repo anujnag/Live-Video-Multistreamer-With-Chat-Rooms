ef64ed49702c5dd6c7ca5f6f0be368e123745dcd
Anuj Nagpal <anujnag@iitk.ac.in>
2017-11-10 22:36:54 +0530

    Removed unnecessary file

--- client.py
+++ client.py
@@ -1,9 +1,24 @@
-def client():
-	print 1
+#!/usr/bin/env python
 
-def main():
-	client()
+from flask import Flask, render_template, Response
+from server import VideoCamera
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    main()
-
+    app.run(host='0.0.0.0', debug=True)
--- server.py
+++ server.py
@@ -3,11 +3,8 @@ import cv2
 class VideoCamera(object):
     def __init__(self):
         # Using OpenCV to capture from device 0. If you have trouble capturing
-        # from a webcam, comment the line below out and use a video file
-        # instead.
+        # from a webcam
         self.video = cv2.VideoCapture(0)
-        # If you decide to use video.mp4, you must have this file in the folder
-        # as the main.py.
         # self.video = cv2.VideoCapture('Metallica.mp4')
     
     def __del__(self):
